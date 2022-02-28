1badecde2574b487ad7a5139376db6006b44e7c2
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-28 04:55:07 +0100

    Update file

--- navbar-app/src/Navbar.js
+++ navbar-app/src/Navbar.js
@@ -1,9 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  
+
   return <h4>navbar</h4>
 }
 
