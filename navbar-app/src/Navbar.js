3f1d162c8253cf4bde07fc36f41437bcb22c2589
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-28 05:03:17 +0100

    Update file

--- navbar-app/src/Navbar.js
+++ navbar-app/src/Navbar.js
@@ -7,7 +7,20 @@ const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
   const linksContainerRef = useRef(null);
   const linksRef = useRef(null);
-  
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  });
 
   return <h4>navbar</h4>
 }
