efb4e3778b532d1fac348932a582681ef6aee3ce
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-01 17:27:20 +0100

    Update file

--- navbar-app/src/Navbar.js
+++ navbar-app/src/Navbar.js
@@ -42,12 +42,20 @@ const Navbar = () => {
               );
             })}
           </ul>
-          <ul>
-            
+          <ul className='social-icons'>
+            {social.map((socialIcon) => {
+              const { id, url, icon } = socialIcon;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
-    </nav>)
-}
+    </nav>
+  );
+};
 
 export default Navbar
